a1b2d4a0702fdc374643a08babddd7c43c868368
Srijan703 <srijanpokhrel343@gmail.com>
2024-12-22 12:39:52 +0545

    api route setup and bot start

--- api/index.js
+++ api/index.js
@@ -1,3 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("../config/db");
+const userInfoRoutes = require("../routes/userInfoRoutes");
+const referralRoutes = require("../routes/referralRoutes");
+
+const startBot = require("../bots/bot"); //tg bot run here
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(
+  cors({
+    origin: ["https://tg_referral_system_ciz.vercel.app"],
+  })
+);
+app.use(express.json());
+
+// Register  API routes
+app.use("/api/user-info", userInfoRoutes);
+app.use("/api/referrals", referralRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
